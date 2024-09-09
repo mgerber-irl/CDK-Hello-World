cfbfa60201882e91acea9a9a9ec9c83ffeb02721
Michael Gerber <michael.gerber@eureka.ie>
2024-09-09 16:29:18 +0100

    Initial commit

---
+++ src/AwsCdkCsharp/AwsCdkCsharpStack.cs
@@ -0,0 +1,13 @@
+using Amazon.CDK;
+using Constructs;
+
+namespace AwsCdkCsharp
+{
+    public class AwsCdkCsharpStack : Stack
+    {
+        internal AwsCdkCsharpStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
+        {
+            // The code that defines your stack goes here
+        }
+    }
+}
